0ed20efa1d026e910932044dd1d0146ce89c8cf6
Mecora <i_akio@126.com>
2024-02-29 23:46:36 +0800

    feat: 修改内容

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+dist_path=docs/.vuepress/dist
+push_address=https://github.com/GitFish123/GitFish123.github.io.git
+# push_address=git@github.com:GitFish123/GitFish123.github.io.git
+remote_branch=gh-pages
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git add -A
+git commit -m "deploy static site"
+git push -f $push_address HEAD:$remote_branch
+
+cd -
+rm -rf $dist_path
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Mecora i_akio@126.com
+ * @Date: 2024-02-29 23:12:39
+ * @LastEditors: Mecora i_akio@126.com
+ * @LastEditTime: 2024-02-29 23:21:00
+ * @FilePath: /GitFish123.github.io/docs/.vuepress/config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+module.exports = {
+  title: '知识空间', // 网站的标题，它将会被用作所有页面标题的前缀，同时，默认主题下，它将显示在导航栏（navbar）上。
+  description: 'Just playing around'
+}
