31f7926e996c9785459cf2f99399ecb363b402bd
Mecora <i_akio@126.com>
2024-03-01 21:40:42 +0800

    feat: add vuepress-plugin-zooming

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: Mecora i_akio@126.com
  * @Date: 2024-02-29 23:12:39
  * @LastEditors: Mecora i_akio@126.com
- * @LastEditTime: 2024-03-01 20:35:24
+ * @LastEditTime: 2024-03-01 21:38:53
  * @FilePath: /GitFish123.github.io/docs/.vuepress/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -61,17 +61,30 @@ module.exports = defineConfig({
     lineNumbers: true, // 代码块显示行号
   },
   plugins: [
-    '@vuepress/nprogress',
-    '@vuepress/back-to-top',
+    '@vuepress/nprogress', // 加载进度条
+    '@vuepress/back-to-top', // 返回顶部
     '@vuepress/active-header-links', {
       sidebarLinkSelector: '.sidebar-link',
       headerAnchorSelector: '.header-anchor'
     },
+    // 点击复制
     'one-click-copy', {
       copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
       copyMessage: '复制成功!', // default is 'Copied successfully!'
       toolTipMessage: 'Copy to clipboard', // default is ''Copy to clipboard'
       duration: 300, // prompt message display time
-    }
+    },
+    // 放大图片
+    [
+      'vuepress-plugin-zooming',
+      {
+        // selector: '.my-wrapper .my-img',
+        delay: 1000,
+        options: {
+          bgColor: 'black',
+          zIndex: 10000,
+        },
+      },
+    ],
   ]
 })
